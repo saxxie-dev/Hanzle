7278cab94d1343839e3e5fcdb4d521580b02885f
saxxie-dev <c@saxxie.dev>
2023-01-20 15:13:01 -0800

    Test out static rendering to gh-pages

---
+++ docs/build/q-895665e0.js
@@ -0,0 +1,1 @@
+import{c as o,T as _,_ as t}from"./q-acba68ae.js";const s=o(_(()=>t(()=>import("./q-11c40952.js"),["build/q-11c40952.js","build/q-acba68ae.js","build/q-e57ae540.js"]),"s_3sccYCDd1Z0"));export{s as default};
