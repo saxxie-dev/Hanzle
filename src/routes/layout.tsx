709e6d437366c335ed725ec3a9da35086faf910c
saxxie-dev <c@saxxie.dev>
2023-01-20 16:18:20 -0800

    Test loading of candidates.json

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,17 @@
-import { component$, Slot } from '@builder.io/qwik';
+import { component$, Slot, useClientEffect$, useStore } from '@builder.io/qwik';
 import Header from '../components/header/header';
+import * as rawData$ from '../candidates.json';
 
 export default component$(() => {
+  const data$: Record<string, string> = (rawData$ as any).default;
+  const store = useStore({
+    char: "",
+  })
+  useClientEffect$(() => {
+    const keys = Object.keys(data$);
+    const char = data$[keys[Math.floor(keys.length * Math.random())]];
+    store.char = char;
+  });
   return (
     <>
       <main>
@@ -12,7 +22,7 @@ export default component$(() => {
       </main>
       <footer>
         <a href="https://www.builder.io/" target="_blank">
-          Made with ♡ by Builder.io
+          Made with ♡ by Builder.io {store.char}
         </a>
       </footer>
     </>
